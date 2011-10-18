94da694003d05b3966b1c7a047c2508d601891bd
Manin Iliy <bkmzvf@gmail.com>
2011-10-18 14:13:01 +0700

    simple subdivision show

--- app/admin/subdivisions.rb
+++ app/admin/subdivisions.rb
@@ -1,8 +1,21 @@
 ActiveAdmin.register Subdivision do
   filter :title
   filter :updated_at
-  index :as => :blog do
-    title :title_with_abbr
+
+  index do
+    column :title do |subdivision|
+      link_to subdivision.title, admin_subdivision_path(subdivision)
+    end
+    column :abbr
+    column :updated_at
   end
-  show :title => proc{subdivision.title}
+
+  show :title => proc { subdivision.title } do
+    render 'subdivision'
+  end
+
+  form do |f|
+    f.inputs :title, :abbr
+    f.buttons
+  end
 end
