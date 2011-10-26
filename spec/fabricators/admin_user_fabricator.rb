a23c35f654c5da2e2f079a2f02d9f2dd8b409757
Evgeniy Serykh <e.v.serykh@gmail.com>
2011-10-26 17:18:18 +0700

    added name to admin_user factory

--- spec/fabricators/admin_user_fabricator.rb
+++ spec/fabricators/admin_user_fabricator.rb
@@ -7,4 +7,5 @@ Fabricator(:admin_user) do
   email                 { Forgery(:internet).email_address }
   password              { Forgery(:basic).password }
   password_confirmation { |user| user.password }
+  name                  { Ryba::Name.full_name }
 end
