b623a8aea355abd5d74f4c9c424f336966e3c1ff
Evgeniy Serykh <e.v.serykh@gmail.com>
2011-10-31 12:41:10 +0700

    RF: subdivsion -> category

--- app/admin/active_admin/view_helpers.rb
+++ app/admin/active_admin/view_helpers.rb
@@ -4,12 +4,4 @@ module ActiveAdmin::ViewHelpers
       content_tag(:div, link_to(category.title, admin_category_path(category)), :class => 'category') + (child_categories.empty? ? '' : content_tag(:div, render_tree(child_categories), :class => 'nested_categories'))
     end.join.html_safe
   end
-
-  def render_categories_tree(category)
-    result = content_tag "h#{category.depth+1}", link_to(category.title, admin_category_path(category))
-    category.children.where(:type => nil).each do |subcategory|
-      result += render_categories_tree(subcategory)
-    end
-    result.html_safe
-  end
 end
