c6dd2b54e8f137bbe617ffaa52670630a4e7088f
Evgeniy Serykh <e.v.serykh@gmail.com>
2011-11-02 14:59:27 +0700

    #8643. пароль на форме редактирования

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -2,12 +2,19 @@ class AdminUser < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :category_ids
 
+  validates :email,
+            :presence => true,
+            :uniqueness => true,
+            :format => { :with => Devise.email_regexp }
+
   validates_presence_of :name
+
+  validates_presence_of :password, :password_confirmation, :unless => :encrypted_password?
 
   has_and_belongs_to_many :categories
 
