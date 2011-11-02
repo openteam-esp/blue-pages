5abe5bba8134f51704e7db805a964eb9341be027
Dmitry Lihachev <lda@openteam.ru>
2011-11-02 09:41:14 +0700

    reduce logging in dev mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,6 +18,8 @@ BluePages::Application.configure do
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
+  # Reduce logging
+  config.log_level = :warn
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
