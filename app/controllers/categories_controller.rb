8bae9b2fb3feee5477e7b88f80e81e03b9e4b122
Evgeniy Serykh <e.v.serykh@gmail.com>
2011-11-03 12:32:30 +0700

    #8653. потерялся переход на управление с публичного вида

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,11 @@
 class CategoriesController < ApplicationController
-
   inherit_resources
 
   actions :show
 
   layout "public/main"
+
+  def current_ability
+    @current_ability ||= AdminAbility.new(current_admin_user)
+  end
 end
