6421103f65023b3873d349266bd84e3b429effdb
Manin Iliy <bkmzvf@gmail.com>
2011-12-06 12:24:12 +0700

    #8891. Запилить ckeditor+el_vfs_client

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,3 +7,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+var CKEDITOR_BASEPATH = '/assets/ckeditor/';
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 BluePages::Application.routes.draw do
   ActiveAdmin.routes(self) unless Rails.env.test?
 
+  mount ElVfsClient::Engine => '/'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
 
   resources :categories, :only => [:index, :show]
