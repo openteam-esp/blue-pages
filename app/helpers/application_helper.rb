7f6f50916c44ab6744267c1a7d657a587a795a7f
Evgeny Lapin <lev@openteam.ru>
2011-12-12 09:54:47 +0700

    первое приближение без active_admin

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,31 @@ module ApplicationHelper
     return resource.address if resource.parent.is_a?(Category)
     parent_address = resource.parent.address
   end
+
+  def title
+    action_title
+  end
+
+  def pluralized_model(controller_name = params[:controller])
+    "pluralize.#{controller_name.to_s.singularize}"
+  end
+
+  def action_title(options={})
+    controller_name = options[:controller] || params[:controller]
+    action = options[:action] || params[:action]
+    action_conf = case action
+      when 'show'
+        return resource
+      when 'create'
+        { :action => 'new', :count => 1 }
+      when 'update'
+        { :action => 'edit', :count => 1 }
+      when 'index'
+        { :action => action, :count => 10 }
+      else
+        { :action => action, :count => 2 }
+      end
+    "#{I18n.t("actions.#{action_conf[:action]}")} #{I18n.t(pluralized_model(controller_name), :count => action_conf[:count])}"
+  end
+
 end
