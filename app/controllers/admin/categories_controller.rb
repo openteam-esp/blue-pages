72502bb1369eaafc0a21958750719a1b8fe8961f
Evgeny Lapin <lev@openteam.ru>
2011-12-12 11:43:53 +0700

    #8926. починить сортировку

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,5 @@
 class Admin::CategoriesController < Admin::ApplicationController
+
   belongs_to :parent_category, :class_name => 'Category', :optional => true, :param => :category_id
 
   def index
@@ -13,8 +14,8 @@ class Admin::CategoriesController < Admin::ApplicationController
         category = Category.find(id)
         category.update_attribute(:position, index.to_i+1)
       end
-
-      return :nothing => true, :status => 200
+      render :nothing => true, :status => 200 and return
     end
   end
+
 end
