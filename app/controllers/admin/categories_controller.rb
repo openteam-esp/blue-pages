6bc3ef0874e4a3c9fa30437c5da3194253e25c3d
Evgeniy Serykh <e.v.serykh@gmail.com>
2011-12-12 14:11:23 +0700

    little fix

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,21 +1,18 @@
 class Admin::CategoriesController < Admin::ApplicationController
-
-  belongs_to :parent_category, :class_name => 'Category', :optional => true, :param => :category_id
+  belongs_to :parential_category, :class_name => 'Category', :optional => true, :param => :category_id
 
   def index
-    index! {
-      @categories = Category.roots
-    }
+    index! { @categories = Category.roots }
   end
 
   def sort
     index! do
       params[:ids].each_with_index do |id, index|
         category = Category.find(id)
-        category.update_attribute(:position, index.to_i+1)
+        category.update_attribute(:position, index.to_i + 1)
       end
+
       render :nothing => true, :status => 200 and return
     end
   end
-
 end
