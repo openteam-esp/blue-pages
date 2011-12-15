817d98176e3037be51f005356988b66dba9e84b6
Dmitry Lihachev <lda@openteam.ru>
2011-12-15 11:18:20 +0700

    regenerate user model and migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,7 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable,
-         :recoverable, :rememberable, :trackable
+  devise :omniauthable, :trackable, :timeoutable
 
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :category_ids
-
-  validates :email,
-            :presence => true,
-            :uniqueness => true,
-            :format => { :with => Devise.email_regexp }
-
-  validates_presence_of :name
-
-  validates_presence_of :password, :password_confirmation, :unless => :encrypted_password?
+  attr_accessible :category_ids
 
   has_and_belongs_to_many :categories
 
@@ -29,25 +16,39 @@ class User < ActiveRecord::Base
   def display_name
     name
   end
+
+  def self.from_omniauth(hash)
+    User.find_or_initialize_by_uid(hash['uid']).tap do |user|
+      user.update_attributes hash['info']
+    end
+  end
 end
+
+
 
 # == Schema Information
 #
 # Table name: users
 #
-#  id                     :integer         not null, primary key
-#  email                  :string(255)     default(""), not null
-#  encrypted_password     :string(128)     default(""), not null
-#  reset_password_token   :string(255)
-#  reset_password_sent_at :datetime
-#  remember_created_at    :datetime
-#  sign_in_count          :integer         default(0)
-#  current_sign_in_at     :datetime
-#  last_sign_in_at        :datetime
-#  current_sign_in_ip     :string(255)
-#  last_sign_in_ip        :string(255)
-#  created_at             :datetime
-#  updated_at             :datetime
-#  name                   :string(255)
+#  id                 :integer         not null, primary key
+#  uid                :string(255)
+#  name               :text
+#  email              :text
+#  nickname           :text
+#  first_name         :text
+#  last_name          :text
+#  location           :text
+#  description        :text
+#  image              :text
+#  phone              :text
+#  urls               :text
+#  raw_info           :text
+#  sign_in_count      :integer         default(0)
+#  current_sign_in_at :datetime
+#  last_sign_in_at    :datetime
+#  current_sign_in_ip :string(255)
+#  last_sign_in_ip    :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
 #
 
