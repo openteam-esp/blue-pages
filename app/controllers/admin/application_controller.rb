d1a0da1d14c8f5c5a3f594cb0c1cd625fb7dfac2
Dmitry Lihachev <lda@openteam.ru>
2012-01-10 11:07:00 +0700

    #9015. авторизация одминских на контроллерах

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,5 +1,6 @@
 class Admin::ApplicationController < ApplicationController
-  inherit_resources
   before_filter :authenticate_user!
   layout 'admin/with_tree'
+  inherit_resources
+  load_and_authorize_resource :except => :index
 end
