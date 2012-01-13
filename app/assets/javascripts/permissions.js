4e160cf41a5f89ee8e9b683019140f05ae802b77
Manin Iliy <bkmzvf@gmail.com>
2012-01-13 10:46:42 +0700

    slidetoggler

--- app/assets/javascripts/permissions.js
+++ app/assets/javascripts/permissions.js
@@ -1,6 +1,28 @@
+//= require jquery.noisy.min
+
 $(function(){
+
+  $('body').noisy({
+        'intensity' : 1,
+      'size' : 200,
+      'opacity' : 0.08,
+      'fallback' : '',
+      'monochrome' : false
+  }).css('background-color', '#EDEBDE');
+
   $('.show_permissions').click(function(){
-    $('.permission_list').slideUp();
-    $(this).parent().next('.permission_list').slideDown();
+    var $this = $(this);
+    var this_permission_list = $this.parent().next('.permission_list');
+
+    $('.permission_list').not(this_permission_list).slideUp();
+    $('.show_permissions').not($this).removeClass('arrowup active').addClass('arrowdown').text('Показать права доступа');
+
+    this_permission_list.slideToggle('slow',function(){
+      if ($(this).is(':visible')) {
+        $this.removeClass('arrowdown').addClass('arrowup active').text('Скрыть права доступа');
+      }else{
+        $this.removeClass('arrowup active').addClass('arrowdown').text('Показать права доступа');
+      };
+    });
   });
 });
