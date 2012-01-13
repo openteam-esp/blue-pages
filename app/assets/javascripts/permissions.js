e895d61c346baf4376852f35dca86702fb35f134
Manin Iliy <bkmzvf@gmail.com>
2012-01-13 12:03:06 +0700

    some beautifuls :)

--- app/assets/javascripts/permissions.js
+++ app/assets/javascripts/permissions.js
@@ -1,27 +1,26 @@
 //= require jquery.noisy.min
 
 $(function(){
-
-  $('body').noisy({
-        'intensity' : 1,
-      'size' : 200,
-      'opacity' : 0.08,
-      'fallback' : '',
-      'monochrome' : false
-  }).css('background-color', '#EDEBDE');
+  $('.container').noisy({
+    'intensity' : 1,
+    'size' : 200,
+    'opacity' : 0.08,
+    'fallback' : '',
+    'monochrome' : false
+  });
 
   $('.show_permissions').click(function(){
     var $this = $(this);
     var this_permission_list = $this.parent().next('.permission_list');
 
     $('.permission_list').not(this_permission_list).slideUp();
-    $('.show_permissions').not($this).removeClass('arrowup active').addClass('arrowdown').text('Показать права доступа');
+    $('.show_permissions').not($this).removeClass('active').html('&darr;&nbsp;Показать права доступа');
 
     this_permission_list.slideToggle('slow',function(){
       if ($(this).is(':visible')) {
-        $this.removeClass('arrowdown').addClass('arrowup active').text('Скрыть права доступа');
+        $this.addClass('active').html('&uarr;&nbsp;Скрыть права доступа');
       }else{
-        $this.removeClass('arrowup active').addClass('arrowdown').text('Показать права доступа');
+        $this.removeClass('active').html('&darr;&nbsp;Показать права доступа');
       };
     });
   });
