bf23e1643af59e4ba79d079f15e2e24bfd69bf86
Dmitry Lihachev <lda@openteam.ru>
2012-01-25 12:03:44 +0700

    #9122. пофиксить путь к bash для FreeBSD

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,4 @@
-set :job_template, "/usr/bin/bash -l -c ':job'" if RUBY_PLATFORM =~ /freebsd/
+set :job_template, "/usr/local/bin/bash -l -c ':job'" if RUBY_PLATFORM =~ /freebsd/
 
 every :day, :at => '5am' do
   rake 'generate_pdf'
