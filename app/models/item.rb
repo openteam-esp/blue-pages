8c4ccce24bf35bdcfaa8dda1b35b3cf705218220
Manin Iliy <bkmzvf@gmail.com>
2012-08-20 16:15:43 +0700

    вводим дополнительные поля: научное звание, научная степень

--- app/models/item.rb
+++ app/models/item.rb
@@ -86,6 +86,8 @@ class Item < ActiveRecord::Base
     result['surname'] = person.surname
     result['name'] = person.name
     result['patronymic'] = person.patronymic
+    result['academic_degree'] = person.academic_degree                             if Settings['app.academical_attributes']
+    result['academic_rank'] = person.academic_rank                                 if Settings['app.academical_attributes']
     result['dossier'] = person.dossier                                             if person.info_path
     result['image_url'] = image_url                                                if image_url.present?
 
