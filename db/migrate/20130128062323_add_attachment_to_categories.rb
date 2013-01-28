8d5aa0a719c6b0234d35dc2664cf3b7b4fc3cfff
Dmitry Lihachev <lda@openteam.ru>
2013-01-28 14:34:48 +0700

    update empty image_urls to null in innorganizations

--- db/migrate/20130128062323_add_attachment_to_categories.rb
+++ db/migrate/20130128062323_add_attachment_to_categories.rb
@@ -1,8 +1,16 @@
 class AddAttachmentToCategories < ActiveRecord::Migration
-  def change
+  def up
     add_column :categories, :image_file_name,    :string
     add_column :categories, :image_content_type, :string
     add_column :categories, :image_file_size,    :integer
     add_column :categories, :image_updated_at,   :datetime
+    Innorganization.where(:image_url => '').update_all(:image_url => nil)
+  end
+
+  def down
+    remove_column :categories, :image_file_name
+    remove_column :categories, :image_content_type
+    remove_column :categories, :image_file_size
+    remove_column :categories, :image_updated_at
   end
 end
