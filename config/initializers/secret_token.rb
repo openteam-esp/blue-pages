7650d615b591f49510ea0d75b075903d043109d4
Dmitry Lihachev <lda@openteam.ru>
2013-03-14 11:57:24 +0700

    remove secret_token from git

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-BluePages::Application.config.secret_token = 'de8f3d0846788d6680cf29bf470fc1bf1b1b021d80aadcf939dcbc20cfe11587cd5201f3e30f483bf863dd09573461bf0dd4b89d088bdc04a317e17a1f63d47b'
