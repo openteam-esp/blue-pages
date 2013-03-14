3eb9de90291b4087e4e2f44fe6e3cba6188fe9e4
Evgeniy Serykh <e.v.serykh@gmail.com>
2013-03-14 11:54:08 +0700

    Fix permissions

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,62 +1,69 @@
+# encoding: utf-8
+
 class Ability
   include CanCan::Ability
+
+  def blue_pages
+    @blue_pages ||= Category.find_by_title('Телефонный справочник')
+  end
+
+  def manager_of?(user, category)
+    category.path.each { |category| return true if user.manager_of?(category) }
+
+    false
+  end
+
+  def operator_of?(user, category)
+    category.path.each { |category| return true if user.operator_of?(category) }
+
+    false
+  end
+
+  def editor_of?(user, category)
+    category.path.each { |category| return true if user.editor_of?(category) }
+
+    false
+  end
 
   def initialize(user)
     return unless user
 
-    can :manage, :all
+    can :manage, :all if user.manager_of?(blue_pages)
 
-    ## common
-    #can :manage, Category do | context |
-      #user.manager_of? context
-    #end
+    can :manage, :application if user.permissions.any?
 
-    #can :manage, Permission do | permission |
-      #permission.context && user.manager_of?(permission.context)
-    #end
+    can :read, User if user.manager?
 
-    #can [:new, :create], Permission do | permission |
-      #!permission.context && user.manager?
-    #end
+    can :manage, Category do |category|
+      manager_of?(user, category)
+    end
 
-    #can [:search, :index], User do
-      #user.manager?
-    #end
+    can :manage, Permission do |permission|
+      manager_of?(user, permission.context)
+    end
 
-    #can :manage, :application do
-      #user.have_permissions?
-    #end
+    can :create, Permission if user.manager?
 
-    #can :manage, :permissions do
-      #user.manager?
-    #end
+    alias_action :create, :read, :update, :destroy, :treeview, :sort, :to => :modify
 
-    #can :manage, :audits do
-      #user.manager_of? Category.first
-    #end
+    can :manage, Category do |category|
+      editor_of?(user, category)
+    end
 
-    ## app specific
+    can :modify, Category do |category|
+      operator_of?(user, category)
+    end
 
-    #alias_action :create, :read, :update, :destroy, :treeview, :sort, :to => :modify
+    can :manage, Item do |item|
+      manager_of?(user, item.itemable)
+    end
 
-    #can :manage, Category do | category |
-      #user.editor_of? category
-    #end
+    can :manage, Item do |item|
+      editor_of?(user, item.itemable)
+    end
 
-    #can :modify, Category do | category |
-      #user.operator_of? category
-    #end
-
-    #can :manage, Item do | item |
-      #user.manager_of? item.itemable
-    #end
-
-    #can :manage, Item do | item |
-      #user.editor_of? item.itemable
-    #end
-
-    #can :modify, Item do | item |
-      #user.operator_of? item.itemable
-    #end
+    can :modify, Item do |item|
+      operator_of?(user, item.itemable)
+    end
   end
 end
