c7b6b9c58f44d8e316c0f7df628d8478ac92e80a
Evgeniy Serykh <e.v.serykh@gmail.com>
2013-03-14 11:33:58 +0700

    Store permissions right

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -1,8 +1,24 @@
 class Permission < ActiveRecord::Base
+  attr_accessible :context_string, :role, :context_id
+  attr_accessor :context_string
+
+  validates_presence_of :context_string
+
   sso_auth_permission roles: [:manager, :operator, :editor]
 
   extend Enumerize
   enumerize :role, in: [:manager, :operator, :editor]
+
+  before_create :set_context_type_and_id
+
+  private
+
+  def set_context_type_and_id
+    context_type, context_id = context_string.split('_')
+
+    self.context_type = context_type.classify
+    self.context_id = context_id
+  end
 end
 
 # == Schema Information
