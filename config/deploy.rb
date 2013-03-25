31e3527978b772a474e6a7dc0ae5ac19ee5ff163
Dmitry Lihachev <lda@openteam.ru>
2013-03-25 14:38:57 +0700

    update openteam-capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 set :default_stage, :ato
 
-require 'openteam/capistrano'
+require 'openteam/capistrano/recipes'
 require 'whenever/capistrano'
 
 set :shared_children, fetch(:shared_children) + %w[public/government.pdf public/blue_pages.pdf]
