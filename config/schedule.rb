8632418a3e64fb05f29cbf646fbd484457af7b57
Evgeny Lapin <lev@openteam.ru>
2013-12-23 11:37:40 +0700

    remove link to pdf

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,7 +2,3 @@ log_dir       = File.expand_path('../../log', __FILE__)
 path_to_bash  = (RUBY_PLATFORM =~ /freebsd/) ? '/usr/local/bin/bash' : '/bin/bash'
 
 set :job_template, "#{path_to_bash} -l -i -c ':job' 1>>#{log_dir}/schedule.log 2>>#{log_dir}/schedule-errors.log"
-
-every :day do
-  rake 'cron'
-end
