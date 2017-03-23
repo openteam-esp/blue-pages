8dd63453115d64db948caa73dc5254cb476e1bb6
Evgeniy Lapin <lev@openteam.ru>
2017-03-23 16:31:36 +0700

    fixed syntax. again

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,11 @@
 require 'openteam/capistrano/deploy'
 
-append :linked_files, %w{
-  config/sunspot.yml
-  public/government.pdf
-  public/blue_pages.pdf
-  public/family_department.pdf
-  public/zdrav_department.pdf
-}
+append :linked_files,
+  'config/sunspot.yml',
+  'public/blue_pages.pdf',
+  'public/family_department.pdf',
+  'public/government.pdf',
+  'public/zdrav_department.pdf'
 
 set :slackistrano,
   channel: (Settings['slack.channel'] rescue ''),
